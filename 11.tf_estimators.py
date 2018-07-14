5874fba6a6fe4d3b9f3a16c738a1983ed24eec03
mkumar73 <manishkr7003@gmail.com>
2018-07-14 20:14:16 +0200

    comments added

--- 11.tf_estimators.py
+++ 11.tf_estimators.py
@@ -6,6 +6,10 @@ tf updates:
 
 tf.examples.tutorials.mnist is deprecated. We will use tf.keras.datasets.mnist instead.
 the tf.contrib.learn API was promoted to tf.estimators and tf.feature_columns
+
+tf estimator is one stop shop for building and training ANN.
+It is a high level API build on top of tf base class.
+
 """
 
 
