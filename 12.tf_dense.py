cbdd9b51e2564a532421d4950bf7b4b0172b0430
mkumar73 <manishkr7003@gmail.com>
2018-07-15 23:22:34 +0200

    tf dense implementation for MNIST

--- 12.tf_dense.py
+++ 12.tf_dense.py
@@ -1,4 +1,14 @@
 import tensorflow as tf
 import numpy as np
 
+n_inputs = 28*28
+n_hidden1 = 300
+n_hidden2 = 100
+n_outputs = 10
+
+tf.reset_graph()
+
+X = tf.placeholder(tf.float32, shape=(None, n_inputs), name="X")
+y = tf.placeholder(tf.int32, shape=(None), name="y")
+
 
