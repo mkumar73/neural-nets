545c32085d4d008533c565842bef653ba0bdb74a
mkumar73 <manishkr7003@gmail.com>
2018-07-18 17:30:51 +0200

    data investigation

--- cifar_all_in_one/cifar.py
+++ cifar_all_in_one/cifar.py
@@ -15,3 +15,81 @@ import tensorflow as tf
 import numpy as np
 import  matplotlib.pyplot as plt
 import os
+
+
+LOGDIR = "logs/"
+
+
+class CIFAR10():
+
+    def __init__(self, data='cifar'):
+
+        self.data = data
+
+    def _load_data(self, data):
+        """
+        :param data: dataset name
+        :return: dataset and labels for training and test
+        """
+
+        if data=='cifar':
+            (x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar10.load_data()
+        return x_train, y_train, x_test, y_test
+
+    def check_sample_data(self):
+        """
+        :return: print something
+        """
+        train, val, test, _, _, _ = self._train_test_split(_index=5000)
+        print('Size of train, validation and test set:\n')
+        print(train.shape)
+        print(val.shape)
+        print(test.shape)
+        print('Sample data:\n')
+        print(train[:10])
+        return
+
+    def data_investigation(self, x, y, show=False):
+        """
+        function to investigate data using plots
+        :param x: subplot parameter
+        :param y: subplot parameter
+        :return: plot figures with labels
+        """
+        n_images = x*y
+
+        x_train, y_train, x_test, y_test = self._load_data(self.data)
+
+        sample_image = x_train[:n_images]
+        sample_label = y_train[:n_images]
+
+        label_to_word = {
+            0: "Airplane",
+            1: "Autombile",
+            2: "Bird",
+            3: "Cat",
+            4: "Deer",
+            5: "Dog",
+            6: "Frog",
+            7: "Horse",
+            8: "Ship",
+            9: "Truck"
+        }
+
+        fig, axs = plt.subplots(x, y)
+        for i, ax in enumerate(np.reshape(axs, [-1])):
+            ax.imshow(sample_image[i])
+            ax.xaxis.set_visible(False)
+            ax.yaxis.set_visible(False)
+            ax.set_axis_off()
+            ax.set_title(str(int(sample_label[i])) + ':' + label_to_word[int(sample_label[i])])
+            if show:
+                plt.show()
+        return
+
+
+cifar = CIFAR10('cifar')
+# cifar.data_investigation(3, 5)
+
+
+
--- cifar_all_in_one/trial_and_error.py
+++ cifar_all_in_one/trial_and_error.py
@@ -1,11 +1,33 @@
 import tensorflow as tf
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 (x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar10.load_data()
 
-print(x_train.shape)
-print(x_test.shape)
-print(y_test.shape)
-print(y_train.shape)
+# print(x_train.shape)
+# print(x_test.shape)
+# print(y_test.shape)
+# print(y_train.shape)
 
 print(y_test[:10])
+#
+sample_image = x_train[:10]
+sample_label = y_train[:10]
+# print(sample_label[5])
+# print(sample_label.shape)
+
+label_to_word = {
+    0: "Airplane",
+    1: "Autombile",
+    2: "Bird",
+    3: "Cat",
+    4: "Deer",
+    5: "Dog",
+    6: "Frog",
+    7: "Horse",
+    8: "Ship",
+    9: "Truck"
+}
+
+print(label_to_word[int(sample_label[5])])
