07e24e8f4bfc369a905a75dbdcfc2b343ed344b9
mkumar73 <manishkr7003@gmail.com>
2018-08-07 13:35:57 +0200

    package input sequence

--- 14.basic_rnn_cell.py
+++ 14.basic_rnn_cell.py
@@ -47,7 +47,7 @@ print('----------------------------------')
 
 
 ###########################################################
-# Use of RNNN cell in high level API
+# Use of RNN cell in high level API
 # Using BasicRNNCell and static_rnn
 
 tf.reset_default_graph()
@@ -84,3 +84,44 @@ print()
 print('Y2 value for the inputs:\n', Y2_val)
 print('Shape of Y2_val:\n', Y2_val.shape)
 print('----------------------------------')
+
+
+###########################################################
+# Use of RNN cell in high level API
+# Using BasicRNNCell and static_rnn
+# Packaged input sequence for RNN
+
+tf.reset_default_graph()
+
+n_steps = 3
+n_inputs = 3
+n_neurons = 5
+
+g3 = tf.Graph()
+with g3.as_default() as g3:
+    X = tf.placeholder(dtype=tf.float32, shape=[None, n_steps, n_inputs])
+    X_seqs = tf.unstack(tf.transpose(X, perm=[1,0,2]))
+
+    basic_rnn_cell = tf.contrib.rnn.BasicRNNCell(num_units=n_neurons)
+    output_seq, cell_state = tf.contrib.rnn.static_rnn(basic_rnn_cell, X_seqs, dtype=tf.float32)
+
+    output = tf.transpose(tf.stack(output_seq), perm=[1,0,2])
+
+    init = tf.global_variables_initializer()
+    X_batch = np.array([
+        # t = 0      # t = 1    # t = 3
+        [[0, 1, 2], [9, 8, 7], [2, 3, 4]], # instance = 1
+        [[3, 4, 5], [0, 0, 0], [6, 7, 9]], # instance = 1
+        [[6, 7, 8], [6, 5, 4], [5, 7, 8]], # instance = 1
+        [[9, 0, 1], [3, 2, 1], [1, 2, 1]]  # instance = 1
+    ])
+
+
+with tf.Session(graph=g3) as session:
+    init.run()
+    Output_val = session.run(output, feed_dict={X: X_batch})
+
+print('----------------------------------')
+print('Output_val for the packaged inputs:\n',Output_val)
+print('Shape of Output_val:\n', Output_val.shape)
+print()
